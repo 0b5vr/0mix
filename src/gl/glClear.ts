5e77d63b35cbce034d566a61bfe8ffaaf3c7c240
0b5vr <0b5vr@0b5vr.com>
2022-07-12 02:03:07 +0900

    fix: fix glClear issue, depth clear was sometimes not working
    I FORGOT THAT `gl.depthMask` mATTERS
    
    FUCK
    
    https://youtu.be/kVc7W0PE7Zc?t=74

--- src/gl/glClear.ts
+++ src/gl/glClear.ts
@@ -4,5 +4,6 @@ import { GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT } from './constants';
 export function glClear( red = 0.0, green = 0.0, blue = 0.0, alpha = 1.0, depth = 1.0 ): void {
   gl.clearColor( red, green, blue, alpha );
   gl.clearDepth( depth );
+  gl.depthMask( true );
   gl.clear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 }
