517b3b62429a8de3fc83d74cb2fd8b04244bda8b
0b5vr <0b5vr@0b5vr.com>
2023-04-14 04:15:06 +0900

    aesthetics: improve Section3Scene
    Reference of Roadtrip: https://www.youtube.com/watch?v=Kb7ZtsbQNsM
    
    Shoutouts to LJ & Virgill

--- src/nodes/Section3Scene/shaders/section3Background.ts
+++ src/nodes/Section3Scene/shaders/section3Background.ts
@@ -1,6 +1,6 @@
 import { BackgroundDefDrawType } from '../../utils/shaders/buildPlaneBackgroundFrag';
-import { FAR } from '../../../config';
-import { abs, add, defUniformNamed, mad, mul, sin, smoothstep, step, sub, sw, vec3, vec4 } from '../../../shaders/shaderBuilder';
+import { FAR, MUSIC_BPM } from '../../../config';
+import { add, addAssign, cos, def, defUniformNamed, div, fract, length, mad, mix, mul, normalize, smoothstep, step, sub, sw, vec3, vec4 } from '../../../shaders/shaderBuilder';
 import { isectPlane } from '../../../shaders/modules/isectPlane';
 import { phongSpecular } from '../../../shaders/modules/phongSpecular';
 
@@ -15,10 +15,32 @@ export const section3Background: BackgroundDefDrawType = () => {
     );
     const rp = mad( isect, rd, ro );
 
+    const rpt = def( 'vec3', rp );
+    addAssign( sw( rpt, 'x' ), 1.0 );
+    addAssign( sw( rpt, 'y' ), 3.9 );
+    addAssign( sw( rpt, 'z' ), mad( 40.0, fract( mul( time, MUSIC_BPM / 240.0 ) ), -20.0 ) );
+
+    // Ref: https://www.youtube.com/watch?v=Kb7ZtsbQNsM
+    // shoutouts to LJ & Virgill
+    const light = def( 'float', mul(
+      div( 1.0, length( rpt ) ),
+      smoothstep( -0.7, -1.0, sw( normalize( rpt ), 'z' ) ),
+    ) );
+
+    addAssign( sw( rpt, 'x' ), -2.0 );
+    addAssign( light, mul(
+      div( 1.0, length( rpt ) ),
+      smoothstep( -0.7, -1.0, sw( normalize( rpt ), 'z' ) ),
+    ) );
+
+    const line = mul(
+      step( cos( sw( rp, 'x' ) ), -0.95 ),
+      step( cos( sw( rp, 'z' ) ), 0.0 ),
+    );
+
     const shape = mul(
-      10.0,
-      smoothstep( 0.98, 1.0, sin( mad( 0.2, sw( rp, 'z' ), mul( 4.0, time ) ) ) ),
-      smoothstep( 0.5, 0.0, abs( sub( abs( sw( rp, 'x' ) ), 1.0 ) ) ),
+      mix( 0.02, 1.0, light ),
+      mix( 3.0, 30.0, line ),
       step( isect, FAR - 1E-3 ), // plane hit
     );
 
